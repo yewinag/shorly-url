0a853c39371773303a96780aaafd12ec73e6ec19
ye win <yewin.aung@zigway.co>
2022-02-09 12:11:25 +0700

    add basic route & folder structure

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Home from './containers/Home';
+import Listing from './containers/Listing';
+import { Header } from './components/layouts';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/listings" element={<Listing />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
